3c9ea4be33449777de4a0fa5ee936d58cdf6fdc4
chintan-koladiya <chintan.koladiya@scaletech.xyz>
2024-04-18 11:52:15 +0530

    add log

--- event/src/routes/event.route.ts
+++ event/src/routes/event.route.ts
@@ -1,33 +1,11 @@
-import { Router } from 'express';
-import { Request, Response } from 'express';
+import { Request, Response, Router } from 'express';
 
 import { logger } from '../utils/logger.utils';
-import CustomError from '../errors/custom.error';
 
 const eventRouter: Router = Router();
 
 eventRouter.post('/', async (request: Request, response: Response) => {
-  logger.info('Event received');
-  // Check request body
-  if (!request.body) {
-    logger.error('Missing request body.');
-    throw new CustomError(400, 'Bad request: No Pub/Sub message was received');
-  }
-
-  // Check if the body comes in a message
-  if (!request.body.message) {
-    logger.error('Missing body message');
-    throw new CustomError(400, 'Bad request: Wrong No Pub/Sub message format');
-  }
-
-  const payload: any = JSON.parse(
-    Buffer.from(request.body.message.data, 'base64').toString('utf8').trim()
-  ).data;
-
-  logger.info(
-    'Event received, request.body.message.data decoded:' +
-      JSON.stringify(payload)
-  );
+  logger.info('Event received', request.body);
 
   response.status(200);
   response.send();
